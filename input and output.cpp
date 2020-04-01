d9764c22aef9a061e534b0b17f1a4eb08aa8f31d
Bloodraven21 <60031755+Bloodraven21@users.noreply.github.com>
2020-04-02 02:54:46 +0530

    Add files via upload

---
+++ input and output.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+     int num1, num2, num3, sum;
+    // Get values
+    cin >> num1 >> num2 >> num3;
+    // Add values
+    sum = num1 + num2 + num3;
+    // Print sum
+    cout << sum << endl;  
+    return 0;
+}
+
